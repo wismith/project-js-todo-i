d7f8e28347d7373692d4b9103538848cdd1899a2
Will Smith <wismith@davidson.edu>
2020-02-04 11:41:36 -0800

    function to list tasks in terminal

--- todo.js
+++ todo.js
@@ -15,3 +15,16 @@ Keep the responsibilities separated as best you can:
 4. Displaying information to the user
 5. Rather user input and taking the appropriate actions
 */
+let process = require('process');
+let fs = require('fs');
+
+function listTasks() {
+  let tasks = fs.readFileSync('todos.txt','utf-8').split('\n');
+  tasks.pop();  // This is how I'm currently handling the blank last line of the text file. Better way?
+  console.log(tasks);
+  for (let i = 1; i <= tasks.length; i++) {
+    console.log(` ${i}: ${tasks[i-1]}`);
+  }
+}
+
+listTasks();
