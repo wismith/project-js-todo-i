398d5e2d6ca6fab8b8e43cd07232b86313285d7f
Jesse Farmer <jesse@20bits.com>
2020-02-08 16:22:50 -0800

    Add Task, TodoList, and driver code
    A basic OOP implementation of a todo list. The driver code
    illustrates how to use to object interfaces. Command-line input
    and file I/O coming in future commits.

--- todo.js
+++ todo.js
@@ -1,17 +1,35 @@
-/*
-What types of objects do you need?
+let TodoList = require('./lib/TodoList');
+let Task = require('./lib/Task');
 
-List out nouns and verbs involved in creating TODO lists.
-Decide which nouns and verbs you want/need to model.
-The nouns will be your objects and the values the functions.
+function showList(todoList) {
+  for (let pos = 1; pos <= todoList.length(); pos++) {
+    let task = todoList.taskAtPosition(pos);
 
-Write simple functions that work on a few number of well-defined objects.
+    console.log(`${pos}. ${task.toString()}`);
+  }
+}
 
-Keep the responsibilities separated as best you can:
+let list = new TodoList();
 
-1. Representing a real-life todo list as in-memory objects
-2. Manipulating those in-memory objects
-3. Reading and writing from the todos.txt file
-4. Displaying information to the user
-5. Rather user input and taking the appropriate actions
-*/
+list.add(new Task('Walk the dog', false));
+list.add(new Task('Sing a song', false));
+list.add(new Task('Bake a cake', false));
+
+showList(list);
+console.log('-------');
+
+console.log();
+console.log('Marking a task complete...');
+console.log('-------');
+
+list.taskAtPosition(2).markComplete();
+
+showList(list);
+
+console.log();
+console.log('Removing a task...');
+console.log('-------');
+
+list.removeTaskAtPosition(1);
+
+showList(list);
